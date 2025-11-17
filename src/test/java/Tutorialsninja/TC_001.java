bbb06bf9c631b8f274d1b3a73c1622f95be39a33
Dnyaneshwar G. <dnyaneshwarghate1010@gmail.com>
2025-11-17 17:49:57 +0530

    new

--- src/test/java/Tutorialsninja/TC_001.java
+++ src/test/java/Tutorialsninja/TC_001.java
@@ -1,35 +1,32 @@
 package Tutorialsninja;
+
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 import Base.BaseClass;
 
-
 public class TC_001 extends BaseClass {
 
- 
-    @Test
-    public void registerWithFields() {
-    
-        getDriver().get("https://tutorialsninja.com/demo/");
+	@Test
+	public void registerWithFields() {
 
-       
-        getDriver().findElement(By.xpath("//span[text()='My Account']")).click();
+		getDriver().get("https://tutorialsninja.com/demo/");
 
-       
-       getDriver().findElement(By.linkText("Register")).click();
-        getDriver().findElement(By.id("input-firstname")).sendKeys("Ashok");
-        getDriver().findElement(By.id("input-lastname")).sendKeys("Tester");
-        getDriver().findElement(By.id("input-email")).sendKeys("ashok" + System.currentTimeMillis() + "@mail.com");
-        getDriver().findElement(By.id("input-telephone")).sendKeys("9876543210");
-        getDriver().findElement(By.id("input-password")).sendKeys("test123");
-        getDriver().findElement(By.id("input-confirm")).sendKeys("test123");
+		getDriver().findElement(By.xpath("//span[text()='My Account']")).click();
 
-        getDriver().findElement(By.xpath("//input[@name='agree']")).click();
-        getDriver().findElement(By.xpath("//input[@value='Continue']")).click();
+		getDriver().findElement(By.linkText("Register")).click();
+		getDriver().findElement(By.id("input-firstname")).sendKeys("Ashok");
+		getDriver().findElement(By.id("input-lastname")).sendKeys("Tester");
+		getDriver().findElement(By.id("input-email")).sendKeys("ashok" + System.currentTimeMillis() + "@mail.com");
+		getDriver().findElement(By.id("input-telephone")).sendKeys("9876543210");
+		getDriver().findElement(By.id("input-password")).sendKeys("test123");
+		getDriver().findElement(By.id("input-confirm")).sendKeys("test123");
 
+		getDriver().findElement(By.xpath("//input[@name='agree']")).click();
+		getDriver().findElement(By.xpath("//input[@value='Continue']")).click();
 
-        
-    }
+		System.out.println("Acccount added sucessfully");
+
+	}
 
 }
