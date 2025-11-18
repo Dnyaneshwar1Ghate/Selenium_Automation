9e57cd7421e0d67fe2b6047f6accb519701be12d
Dnyaneshwar G. <dnyaneshwarghate1010@gmail.com>
2025-11-18 16:45:34 +0530

    hello

--- src/test/java/TestNG/TestNG_TC_001.java
+++ src/test/java/TestNG/TestNG_TC_001.java
@@ -1,6 +1,7 @@
 package TestNG;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -12,21 +13,23 @@ class TestNG_TC_001 extends BaseClass {
 		getDriver().get("https://demoqa.com/");
 		getDriver().findElement(By.xpath("//h5[text()=\"Elements\"]")).click();
 		getDriver().findElement(By.xpath("//span[text()='Text Box']")).click();
-		
-		//userName
+
+		// userName
 		getDriver().findElement(By.id("userName")).sendKeys("abc");
-		//userEmail
+		// userEmail
 		getDriver().findElement(By.id("userEmail")).sendKeys("abc@gmail.com");
-		//currentAddress
+		// currentAddress
 		getDriver().findElement(By.id("currentAddress")).sendKeys("bavdhan pune");
-		//permanentAddress
+		// permanentAddress
 		getDriver().findElement(By.id("permanentAddress")).sendKeys("bavdhan pune");
-		//submit
+		// submit
+
+		JavascriptExecutor js = (JavascriptExecutor) getDriver();
+		js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
 		getDriver().findElement(By.id("submit")).click();
-		
-		String out=getDriver().findElement(By.id("output")).getText();
+
+		String out = getDriver().findElement(By.id("output")).getText();
 		System.out.println(out);
-			
 
 	}
 
