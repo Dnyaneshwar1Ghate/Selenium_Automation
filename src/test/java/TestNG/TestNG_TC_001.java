f5866cc1c20f61d22d24b791e8909a2b6d63b886
Dnyaneshwar G. <dnyaneshwarghate1010@gmail.com>
2025-11-18 16:34:09 +0530

    new

--- src/test/java/TestNG/TestNG_TC_001.java
+++ src/test/java/TestNG/TestNG_TC_001.java
@@ -1,5 +1,6 @@
 package TestNG;
 
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -8,11 +9,24 @@ import Base.BaseClass;
 @Test
 class TestNG_TC_001 extends BaseClass {
 	public void testNG_001_Test() {
-		getDriver().get("https://www.toolsqa.com");
-
-		String testTitle = "Tools QA";
-		String originalTitle = getDriver().getTitle();
-		Assert.assertEquals(originalTitle, testTitle);
+		getDriver().get("https://demoqa.com/");
+		getDriver().findElement(By.xpath("//h5[text()=\"Elements\"]")).click();
+		getDriver().findElement(By.xpath("//span[text()='Text Box']")).click();
+		
+		//userName
+		getDriver().findElement(By.id("userName")).sendKeys("abc");
+		//userEmail
+		getDriver().findElement(By.id("userEmail")).sendKeys("abc@gmail.com");
+		//currentAddress
+		getDriver().findElement(By.id("currentAddress")).sendKeys("bavdhan pune");
+		//permanentAddress
+		getDriver().findElement(By.id("permanentAddress")).sendKeys("bavdhan pune");
+		//submit
+		getDriver().findElement(By.id("submit")).click();
+		
+		String out=getDriver().findElement(By.id("output")).getText();
+		System.out.println(out);
+			
 
 	}
 
